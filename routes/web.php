ad6d31d4dc746baf38d1810034f2386498bb679c
Mark Vonk <markjhvonk@gmail.com>
2017-09-03 22:48:19 +0200

    databse connection + simple demo
    connected the app to database and coded small demo to read some info from the database

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,17 @@
 |
 */
 
-Route::get('/', function () {
+Route::get('/tasks', function () {
     $tasks = DB::table('tasks')->get();
 
-    return view('welcome', compact('tasks'));
+    return view('tasks.index', compact('tasks'));
+});
+
+Route::get('/tasks/{id}', function ($id) {
+    $task = DB::table('tasks')->find($id);
+
+    // dd($task);
+    return view('tasks.show', compact('task'));
 });
 
 Route::get('/about', function () {
